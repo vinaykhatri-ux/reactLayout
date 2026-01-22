e944cb93b8294752bb0f0a9927ce974b711a5ca9
vinay <vinay.khatri@unifyapps.com>
2026-01-22 11:23:27 +0530

    complete gmail and twitter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Gmail from "./layout/Gmail/Gmail";
+import Airbnb from "./layout/Airbnb/Airbnb";
+import Twitter from "./layout/Twitter/Twitter";
 
 function App() {
-  <>
-  </>
+  return (
+    <>
+      <Twitter />
+    </>
+  );
 }
 
-export default App
+export default App;
