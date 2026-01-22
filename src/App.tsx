9dec0e21f436c37e932b2c2f0433e0bf7d31f7ad
vinay <vinay.khatri@unifyapps.com>
2026-01-22 13:13:57 +0530

    complete Salesforce Tableau

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import Gmail from "./layout/Gmail/Gmail";
 import Airbnb from "./layout/Airbnb/Airbnb";
 import Twitter from "./layout/Twitter/Twitter";
 
+import Zillow from "./layout/Zillow/Zillow";
+import Tableau from "./layout/Tableau/Tableau";
+
+import Salesforce from "./layout/Salesforce/Salesforce";
+
 function App() {
   return (
     <>
-      <Twitter />
+      <Salesforce />
     </>
   );
 }
